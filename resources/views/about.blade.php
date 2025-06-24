be5943db90efa0c55786da4bdfb28a7e65722847
kreativaan <ivankovacevic123456@gmail.com>
2025-06-24 14:53:00 +0200

    Added about page with controller and view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About us</title>
+</head>
+
+<body>
+    <h1>This is the About PAge</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit dignissimos voluptate nesciunt. Illo animi dolor
+        totam. Fuga beatae consequuntur numquam nostrum nobis eos eveniet doloremque necessitatibus expedita? Explicabo
+        neque vel sed dicta aut perferendis quisquam nihil velit tempore in ea laudantium similique quam, dolorem
+        consequatur laboriosam placeat nemo ratione reprehenderit quae distinctio veniam exercitationem! Itaque nulla
+        iusto facilis repudiandae tempore velit, quam, labore incidunt eaque molestias voluptate asperiores esse rerum!
+        Sed qui minima dolores blanditiis, sint dignissimos! Natus accusamus libero incidunt. Voluptatibus optio nobis
+        quos reiciendis. Quod amet atque, pariatur ut repellat saepe modi excepturi mollitia inventore dolore in omnis!
+    </p>
+</body>
+
+</html>
